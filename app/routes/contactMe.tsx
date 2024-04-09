8975d41a9c9b5a09f98d817b43bb2e152abf0e87
BrandonWoodruff <bmw132000@gmail.com>
2024-04-08 20:48:43 -0600

    Hopefully this works now.

--- app/routes/contactMe.tsx
+++ app/routes/contactMe.tsx
@@ -20,7 +20,8 @@ const ContactMe: React.FC = () => {
       <header><Head /></header>
       <h1 className="text-3xl font-bold text-white mb-4">Contact Me</h1>
       <br></br>
-      <form name="contact" method="POST" data-netlify="true" action="routes/success" onSubmit={handleSubmit}>
+      <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+        <input type="hidden" name="form-name" value="contact" />
         <div className="mb-4">
           <label className="block text-lg text-white mb-2">Your Name:</label>
           <input type="text" name="name" className="input-box" onChange={(e) => setName(e.target.value)} />
